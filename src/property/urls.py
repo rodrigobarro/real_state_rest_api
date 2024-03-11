69afc789e5e1ad8538819c0f3590aaa1f546a060
Rodrigo Barro <rodrigo.correa.barro@gmail.com>
2024-03-10 22:56:31 -0300

    feat(Property-update): Add property update use case
    BREAKING CHANGE:

--- src/property/urls.py
+++ src/property/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from property import views
 
 urlpatterns = [
-    path('', views.PropertyList.as_view())
+    path('', views.PropertyList.as_view()),
+    path('/<uuid:pk>/', views.PropertyDetail.as_view())
 ]
